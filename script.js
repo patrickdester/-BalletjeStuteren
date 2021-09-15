329409ba92b517f72ed5bb0478093a073d24b69a
SamLouwers <90309503+patrickdester@users.noreply.github.com>
2021-09-15 07:27:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var x;
+var v;
+function setup() {
+  createCanvas(500, 400);
+  v = 5;
+  x = 30;
+}
+
+
+
+function draw() {
+  background(225);
+  fill('blue')
+  circle(x, 130, 20);
+  
+  x = x + v;
+  
+  if (x < 0 || x > 500){ 
+    v = v * -1;
+    }
+ 
+ 
+ }
